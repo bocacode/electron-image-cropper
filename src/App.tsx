7fbec8d2917ce8295b7f556d26bed31417be9ec8
Todd <todd@bocacode.com>
2021-05-16 17:16:09 -0400

    added cropper

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../assets/icon.svg';
-import './App.global.css';
+import React from 'react'
+import Crop from './scenes/Crop'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import icon from '../assets/icon.svg'
+import './App.global.css'
 
 const Hello = () => {
   return (
     <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
+      <div className='Hello'>
+        <img width='200px' alt='icon' src={icon} />
       </div>
       <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
+      <div className='Hello'>
         <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
+          href='https://electron-react-boilerplate.js.org/'
+          target='_blank'
+          rel='noreferrer'
         >
-          <button type="button">
-            <span role="img" aria-label="books">
+          <button type='button'>
+            <span role='img' aria-label='books'>
               📚
             </span>
             Read our docs
           </button>
         </a>
         <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
+          href='https://github.com/sponsors/electron-react-boilerplate'
+          target='_blank'
+          rel='noreferrer'
         >
-          <button type="button">
-            <span role="img" aria-label="books">
+          <button type='button'>
+            <span role='img' aria-label='books'>
               🙏
             </span>
             Donate
@@ -37,15 +38,16 @@ const Hello = () => {
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default function App() {
+export default function App () {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route path='/hello' component={Hello} />
+        <Route path='/' component={Crop} />
       </Switch>
     </Router>
-  );
+  )
 }
---
+++ src/scenes/Crop.js
@@ -0,0 +1,56 @@
+import React, { useState, useCallback } from 'react'
+import Cropper from 'react-easy-crop'
+import { getOrientation } from 'get-orientation/browser'
+
+const ORIENTATION_TO_ANGLE = {
+  '3': 180,
+  '6': 90,
+  '8': -90,
+}
+
+function Crop() {
+  const [imageSrc, setImageSrc] = useState(null)
+  const [crop, setCrop] = useState({ x: 0, y: 0 })
+  const [rotation, setRotation] = useState(0)
+  const [zoom, setZoom] = useState(1)
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
+  const [croppedImage, setCroppedImage] = useState(null)
+
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    console.log(croppedArea, croppedAreaPixels)
+  }, [])
+
+  const onFileChange = async (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0]
+      let imageDataUrl = await readFile(file)
+      // apply rotation if needed
+      const orientation = await getOrientation(file)
+      const rotation = ORIENTATION_TO_ANGLE[orientation]
+      if (rotation) {
+        imageDataUrl = await getRotatedImage(imageDataUrl, rotation)
+      }
+      setImageSrc(imageDataUrl)
+    }
+  }
+  return (
+    <div className="App">
+    {!imageSrc
+      ? <input type="file" onChange={onFileChange} accept="image/*" />
+      : <div className="crop-container">
+          <Cropper
+            image={imageSrc}
+            crop={crop}
+            zoom={zoom}
+            aspect={4 / 3}
+            onCropChange={setCrop}
+            onCropComplete={onCropComplete}
+            onZoomChange={setZoom}
+          />
+        </div>
+      }
+    </div>
+  )
+}
+
+export default Crop
